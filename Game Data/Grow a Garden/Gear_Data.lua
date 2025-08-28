d284dd2cb4d594a44aa017f1a2f4deafae971b7e
Ki Night <kinightrd@gmail.com>
2025-08-28 09:40:09 -0300

    Create Gear_Data.lua

---
+++ Game Data/Grow a Garden/Gear_Data.lua
@@ -0,0 +1,123 @@
+local GearData = {
+    {
+        Name = "Watering Can",
+        Function = "Speeds up plant growth, 10 uses.",
+        CostSheckles = 50000,
+        CostRobux = 39,
+        Rarity = "Common"
+    },
+    {
+        Name = "Trading Ticket",
+        Function = "Allows you to trade with other players (one-time use).",
+        CostSheckles = 100000,
+        CostRobux = 19,
+        Rarity = "Uncommon"
+    },
+    {
+        Name = "Trowel",
+        Function = "Moves Plants, five uses.",
+        CostSheckles = 100000,
+        CostRobux = 49,
+        Rarity = "Uncommon"
+    },
+    {
+        Name = "Recall Wrench",
+        Function = "Teleports to Gear Shop, five uses.",
+        CostSheckles = 150000,
+        CostRobux = 59,
+        Rarity = "Uncommon"
+    },
+    {
+        Name = "Basic Sprinkler",
+        Function = "Increases Growth Speed and Fruit Size, lasts five minutes.",
+        CostSheckles = 25000,
+        CostRobux = 79,
+        Rarity = "Rare"
+    },
+    {
+        Name = "Advanced Sprinkler",
+        Function = "Increases Growth Speed and Mutation chances, lasts five minutes.",
+        CostSheckles = 50000,
+        CostRobux = 99,
+        Rarity = "Legendary"
+    },
+    {
+        Name = "Medium Toy",
+        Function = "Boosts a pet's passive ability.",
+        CostSheckles = 4000000,
+        CostRobux = 189,
+        Rarity = "Legendary"
+    },
+    {
+        Name = "Medium Treat",
+        Function = "Boosts a pet's earned XP.",
+        CostSheckles = 4000000,
+        CostRobux = 199,
+        Rarity = "Legendary"
+    },
+    {
+        Name = "Godly Sprinkler",
+        Function = "Increases Growth Speed, Mutation chances and Fruit Size, lasts five minutes.",
+        CostSheckles = 120000,
+        CostRobux = 149,
+        Rarity = "Mythical"
+    },
+    {
+        Name = "Magnifying Glass",
+        Function = "Inspect plants to reveal the value without collecting them.",
+        CostSheckles = 10000000,
+        CostRobux = 99,
+        Rarity = "Mythical"
+    },
+    {
+        Name = "Master Sprinkler",
+        Function = "Greatly increases Growth Speed, Mutation Chances and Fruit Size, lasts 10 minutes.",
+        CostSheckles = 10000000,
+        CostRobux = 199,
+        Rarity = "Divine"
+    },
+    {
+        Name = "Cleaning Spray",
+        Function = "Cleans mutations off fruit, 10 uses.",
+        CostSheckles = 15000000,
+        CostRobux = 139,
+        Rarity = "Divine"
+    },
+    {
+        Name = "Favourite Tool",
+        Function = "Favourites your fruit plants to prevent collecting, 20 uses.",
+        CostSheckles = 20000000,
+        CostRobux = 119,
+        Rarity = "Divine"
+    },
+    {
+        Name = "Harvest Tool",
+        Function = "Harvests all fruit from a chosen plant, 5 uses.",
+        CostSheckles = 30000000,
+        CostRobux = 149,
+        Rarity = "Divine"
+    },
+    {
+        Name = "Friendship Pot",
+        Function = "A flower pot to share with a friend!",
+        CostSheckles = 15000000,
+        CostRobux = 39,
+        Rarity = "Divine"
+    },
+    {
+        Name = "Grandmaster Sprinkler",
+        Function = "Massively increases growth speed, mutation chance, fruit size, and boosts pets. Lasts 10 mins.",
+        CostSheckles = 1000000000,
+        CostRobux = 279,
+        Rarity = "Prismatic"
+    },
+    {
+        Name = "Level Up Lollipop",
+        Function = "Increases a pet's age by 1.",
+        CostSheckles = 10000000000,
+        CostRobux = 79,
+        Rarity = "Prismatic"
+    }
+}
+
+return GearData
